f3015813d4c0765ea4864f3603ca6fd7b254cdf7
pzc <paul.zimmerman.clayton@gmail.com>
2018-08-30 17:58:20 -0500

    try excepts working to warn of bad json

--- smilewidgets/products/views.py
+++ smilewidgets/products/views.py
@@ -6,18 +6,28 @@ from .models import Product, ProductPrice
 
 
 @csrf_exempt # For testing with Postman
-def get_price(request):
-    # get incoming parameters, save as dictionary/variables
+def get_price(request):    
+    # get incoming parameters as json, save as dictionary/variables
     params = json.loads(request.body)
-    code = params['code']
-    date = params['date']
-
-    # check code and date against changed prices
+    # check incoming keys to make sure they are correct
+    # if they are, continue. else, notify user
+    try:
+        code = params['code']
+        date = params['date']
+    except KeyError:
+        return HttpResponse('Incorrect keys! Make sure you are sending two keys, "dict" and "date"')
     
+    # check code against product price models
+    try:
+        # Makes sure code is valid
+        new_price = model_to_dict(ProductPrice.objects.get(code=code))
+    except ProductPrice.DoesNotExist:
+        # Notify user if code is incorrectly formatted
+        return HttpResponse('The code you submitted is incorrect. Please try again')
 
-    new_price = model_to_dict(ProductPrice.objects.get(code=params['code']))
-    # if match, return new price
-    # else, return old price
-
-
-    return HttpResponse(new_price['price'])
+    # check submitted date against calendar date
+    # try:
+    date_match = model_to_dict(ProductPrice.objects.filter(date_start=date).first())
+    print('date', date_match['date_start'], date)
+    # finally:
+    return HttpResponse('That date didn\'t match!')
